b1aeefe5d904ec4ce70ee823651aca9ada0fb7a3
HarryMeng <harry.mld.2010@gmail.com>
2022-10-20 00:36:50 -0700

    Complete connecting Video List and Player components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,24 +2,33 @@ import exampleVideoData from '../data/exampleVideoData.js';
 import VideoList from './VideoList.js';
 import VideoPlayer from './VideoPlayer.js';
 import Search from './Search.js';
+// import React, { useState } from 'react';
 
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em><Search /></h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> <VideoPlayer video={exampleVideoData[0]} /></h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> <VideoList videos={exampleVideoData} /></h5></div>
+const { useState } = React;
+
+var App = () => {
+
+  const [allVideos, setAllVideos] = useState(exampleVideoData);
+  const [currentVideo, setCurrentVideo] = useState(exampleVideoData[0]);
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="col-md-6 offset-md-3">
+          <div><h5><em>search</em><Search /></h5></div>
+        </div>
+      </nav>
+      <div className="row">
+        <div className="col-md-7">
+          <div><h5><em>videoPlayer</em> <VideoPlayer video= {currentVideo} /></h5></div>
+        </div>
+        <div className="col-md-5">
+          <div><h5><em>videoList</em> <VideoList videos={allVideos} videoClickHandler = {setCurrentVideo} /></h5></div>
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,9 +1,12 @@
 import VideoListEntry from './VideoListEntry.js';
 
 var VideoList = (props) => (
+
   <div className="video-list">
     {props.videos.map((video) => (
-      <VideoListEntry video = {video}/>
+      // {props.videos.map((video, index) => (
+      <VideoListEntry video={video} videoClickHandler={props.videoClickHandler} />
+      // <VideoListEntry video={video} key={index} onClick={e => props.handler(props.videos[index])} />
     ))}
     {/* <div><h5><em>videoListEntry</em> view goes here</h5></div>
     <div><h5><em>videoListEntry</em> view goes here</h5></div>
@@ -18,6 +21,7 @@ var VideoList = (props) => (
 // Warnings will be shown in the console when the defined rules are violated
 VideoList.propTypes = {
   videos: PropTypes.array.isRequired,
+  videoClickHandler: PropTypes.func.isRequired,
 };
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
