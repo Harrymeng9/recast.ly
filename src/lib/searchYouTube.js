329bec244df02a28f8fd142b657a74710e46fdb7
HarryMeng <harry.mld.2010@gmail.com>
2022-10-22 10:28:17 -0700

    Complete creating reusable API helper

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,11 +1,28 @@
 import { API_KEY, YOUTUBE_API_KEY } from '../config/config.js';
+import exampleVideoData from '../data/exampleVideoData.js';
 
 $.ajaxPrefilter(function (settings, _, jqXHR) {
   jqXHR.setRequestHeader('Authorization', API_KEY);
 });
 
 var searchYouTube = (query, callback) => {
-  // TODO
+
+  //https://codepen.io/zackspear/pen/PaGrEv
+  $.get('https://app-hrsei-api.herokuapp.com/api/recastly/videos', {
+    part: 'snippet',
+    q: query,
+    type: 'video',
+    order: 'viewCount',
+    // channelId: channelID,
+    maxResults: 10,
+    videoEmbeddable: true,
+    relevanceLanguage: 'en',
+    key: YOUTUBE_API_KEY
+  },
+  function (data) {
+    callback(data);
+  }
+  );
 };
 
 export default searchYouTube;
